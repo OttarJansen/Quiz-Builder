e19893a273a8b2aa812c6ebdef748e442a901adc
Ottar <ottarkj@uia.no>
2026-01-24 23:49:30 +0100

    moved the files from the middleware repository over to this repository to create a better file structure

--- index.mjs
+++ index.mjs
@@ -1,10 +1,15 @@
-const express = require('express')
+import express from 'express';
+import { validateQuiz } from './middleware/validateQuiz.mjs';
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json());
+
+app.post('/quiz', validateQuiz, (req, res) => {
+  res.status(201).json({ message: "Quiz is valid" });
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
