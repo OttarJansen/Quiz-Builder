d0f8f2236b3fd65b66a038f1d0ed49c76e6212dc
Ottar <ottarkj@uia.no>
2026-03-01 18:27:21 +0100

    Added static to public

--- server.mjs
+++ server.mjs
@@ -10,6 +10,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
 
+app.use(express.static("public"));
 app.use("/quizzes", quizRouter);
 app.use("/user", securityAudit, userRouter);
 
